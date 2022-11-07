77ca89d8239599c22efa63299ae336b03a79a3cf
Matteo Peyronel <28830727+matteopey@users.noreply.github.com>
2022-11-07 11:23:26 +0100

    Graph example with AD

---
+++ Graph/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Graph;
+using Microsoft.Graph.Auth;
+using Microsoft.Identity.Client;
+
+var clientId = "";
+var tenantId = "";
+
+var app = PublicClientApplicationBuilder
+    .Create(clientId)
+    .WithAuthority(AzureCloudInstance.AzurePublic, tenantId)
+    .WithRedirectUri("http://localhost")
+    .Build();
+
+string[] scopes = { "user.read" };
+var result = await app.AcquireTokenInteractive(scopes).ExecuteAsync();
+
+var provider = new InteractiveAuthenticationProvider(app, scopes);
+var client = new GraphServiceClient(provider);
+
+User me = await client.Me.Request().GetAsync();
+Console.WriteLine($"Display name: {me.DisplayName}");
