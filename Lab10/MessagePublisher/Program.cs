4333d3232fd2d9b4f38b9c33415c3007e1cfb4f9
Matteo Peyronel <28830727+matteopey@users.noreply.github.com>
2022-11-08 15:05:47 +0100

    Lab 10 - Service bus

---
+++ Lab10/MessagePublisher/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+using Azure.Messaging.ServiceBus;
+
+const string serviceBusConnectionString = "Endpoint=sb://matteopeysbnamespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=VkVNnPrhst+PvOE9g7suV0f/tsWyFkal6ML2aEBEqZQ=";
+const string queueName = "myqueue";
+const int numOfMessages = 300;
+
+ServiceBusClient client = new ServiceBusClient(serviceBusConnectionString);
+ServiceBusSender sender = client.CreateSender(queueName);
+
+using var messageBatch = await sender.CreateMessageBatchAsync();
+
+for (int i = 1; i <= numOfMessages; i++)
+{
+    if (!messageBatch.TryAddMessage(new ServiceBusMessage($"Message {i}")))
+    {
+        throw new Exception($"The message {i} is too large to fit in the batch.");
+    }
+}
+
+try
+{
+    await sender.SendMessagesAsync(messageBatch);
+    Console.WriteLine($"A batch of {numOfMessages} messages has been published to the queue.");
+}
+finally
+{
+    await sender.DisposeAsync();
+    await client.DisposeAsync();
+}
---
+++ Lab10/MessageReader/Program.cs
@@ -0,0 +1,49 @@
+﻿using Azure.Messaging.ServiceBus;
+
+const string serviceBusConnectionString = "Endpoint=sb://matteopeysbnamespace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=VkVNnPrhst+PvOE9g7suV0f/tsWyFkal6ML2aEBEqZQ=";
+const string queueName = "myqueue";
+
+ServiceBusClient client = new ServiceBusClient(serviceBusConnectionString);
+ServiceBusProcessor processor = client.CreateProcessor(queueName, new ServiceBusProcessorOptions());
+
+try
+{
+    processor.ProcessMessageAsync += MessageHandler;
+    processor.ProcessErrorAsync += ErrorHandler;
+
+    await processor.StartProcessingAsync();
+
+    Console.ReadKey();
+
+    Console.WriteLine("\nStopping the receiver...");
+    await processor.StopProcessingAsync();
+    Console.WriteLine("Stopped receiving messages");
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    await processor.DisposeAsync();
+    await client.DisposeAsync();
+}
+
+async Task MessageHandler(ProcessMessageEventArgs args)
+{
+    var rand = new Random().NextDouble();
+    if (rand > 0.9)
+    {
+        throw new Exception("New exception");
+    }
+
+    string body = args.Message.Body.ToString();
+    Console.WriteLine($"Received: {body}");
+    await args.CompleteMessageAsync(args.Message);
+}
+
+Task ErrorHandler(ProcessErrorEventArgs args)
+{
+    Console.WriteLine(args.Exception.ToString());
+    return Task.CompletedTask;
+}
